c1adc5222cc587f8897b673cfecc25b4f904a04d
Dusty DeWeese <dustin.deweese@gmail.com>
2020-02-17 09:13:31 -0800

    clang format

--- Util.cpp
+++ Util.cpp
@@ -1,30 +1,29 @@
 #include "Util.h"
 
-namespace Gorder
-{
+namespace Gorder {
 
-unsigned long long MyRand64(){
-	unsigned long long ret, tmp;
-	ret=rand();
-	ret=ret<<33;
-	tmp=rand();
-	ret=ret|(tmp<<2);
-	tmp=rand();
-	ret=ret|(tmp>>29);
+unsigned long long MyRand64() {
+  unsigned long long ret, tmp;
+  ret = rand();
+  ret = ret << 33;
+  tmp = rand();
+  ret = ret | (tmp << 2);
+  tmp = rand();
+  ret = ret | (tmp >> 29);
 
-	return ret;
+  return ret;
 }
 
-string extractFilename(const char* filename){
-	string name(filename);
-	int pos=name.find_last_of('.');
+string extractFilename(const char *filename) {
+  string name(filename);
+  int pos = name.find_last_of('.');
 
-	return name.substr(0, pos);
+  return name.substr(0, pos);
 }
 
-void quit(){
-	system("pause");
-	exit(0);
+void quit() {
+  system("pause");
+  exit(0);
 }
 
-}
+} // namespace Gorder
